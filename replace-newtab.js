a62a167e49702284f358a85656fc9bad187488ab
Edward Cheng <c4150221@gmail.com>
2015-09-21 23:32:47 +0800

    init 3

--- replace-newtab.js
+++ replace-newtab.js
@@ -1,1 +1,15 @@
-window.document.write("Works");
+(function (document) {
+  document.write(
+      '<!doctype html>' +
+      '<html>' +
+        '<head>' +
+          '<link rel="stylesheet" type="text/css" href="' + chrome.extension.getURL('container.css') + '">' +
+          '<link rel="import" href="' + chrome.extension.getURL('newtab.html') + '">' +
+        '</head>' +
+        '<body>' +
+          '<pure-newtab></pure-newtab>' +
+        '</body>' +
+      '</html>'
+    );
+  document.close()
+})(window.document);
